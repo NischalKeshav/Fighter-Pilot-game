fc61da8536ab9df08039d764911e2d69bd967ae9
Nischal Keshav <96449844+NischalKeshav@users.noreply.github.com>
2022-03-18 23:24:56 +0000

    commit2

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import turtle
+import random
 from turtle import Screen, Turtle
 screen= Screen()
-turtle.screensize(canvwidth=500, canvheight=300,bg="black")
+screen.tracer(False)
+
+screenWidth = 400
+screenHeight = 400
+
+turtle.screensize(canvwidth=screenWidth, canvheight=screenHeight,bg="black")
+ListOfObjects = []
 class Bomber(Turtle):
-  def __init__(self,xpos,ypos,xSTRECH=1,ySTRECH=1):
+  def __init__(self,xpos,ypos,xSTRECH=1,ySTRECH=1,list= ListOfObjects):
     super().__init__()
+    list.append(self)
     self.penup()
     self.shape("square")
     self.color("white")
     self.goto(xpos,ypos)
-bomber1= Bomber(-200,80)
-bomber2= Bomber(200,80)
-bomber3= Bomber(-200,-80)
-bomber4= Bomber(200,-80)
+    self.yMove = random.randint(-5,5)
+    self.xMove = random.randint(0,10)-self.yMove
+  def move(self):
+    global screenWidth,screenHeight
+    x = self.xcor()+self.xMove
+    y = self.ycor()+self.yMove
+    self.goto(x,y)
+    if x > screenWidth/2 or screenWidth*-1<x:
+      self.xMove = self.xMove*-1
+    if
+bomber1= Bomber(-200,200)
+bomber2= Bomber(200,200)
+bomber3= Bomber(-200,-200)
+bomber4= Bomber(200,-200)
+while True:  
+  for  obj in ListOfObjects:
+    obj.move()
+  screen.update()
 screen.mainloop()
