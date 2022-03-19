f050d2826a2e21ec4c480721c091ee032961aa3d
NischalKeshav <keshavn@bentonvillek12.org>
2022-03-18 19:21:14 -0500

    commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import turtle
 import random
+import time
 from turtle import Screen, Turtle
 screen= Screen()
 screen.tracer(False)
 
-screenWidth = 400
-screenHeight = 400
+screenWidth = 600
+screenHeight = 600
 
-turtle.screensize(canvwidth=screenWidth, canvheight=screenHeight,bg="black")
+turtle.screensize(screenWidth, screenHeight,bg="black")
 ListOfObjects = []
 class Bomber(Turtle):
   def __init__(self,xpos,ypos,xSTRECH=1,ySTRECH=1,list= ListOfObjects):
@@ -17,22 +18,67 @@ class Bomber(Turtle):
     self.shape("square")
     self.color("white")
     self.goto(xpos,ypos)
-    self.yMove = random.randint(-5,5)
-    self.xMove = random.randint(0,10)-self.yMove
+    self.yMove = random.randint(-2,2)
+    if self.yMove==0:
+      self.yMove+=1
+    self.xMove = random.randint(1,8)
   def move(self):
     global screenWidth,screenHeight
-    x = self.xcor()+self.xMove
-    y = self.ycor()+self.yMove
-    self.goto(x,y)
-    if x > screenWidth/2 or screenWidth*-1<x:
+    self.x = self.xcor()+self.xMove
+    self.y = self.ycor()+self.yMove
+    self.goto(self.x,self.y)
+    if self.x > screenWidth/2 or (screenWidth/2)*-1>self.x:
       self.xMove = self.xMove*-1
-    if
+    if self.y > screenHeight/2 or (screenHeight/2)*-1>self.y:
+      self.yMove = self.yMove *-1
+class Player(Turtle):
+  def moveup(self):
+    self.y+=9
+    self.goto(self.x,self.y)
+
+  def movedown(self):
+    self.y -= 9
+    self.goto(self.x, self.y)
+  def moveright(self):
+    self.x += 9
+    self.goto(self.x, self.y)
+  def moveleft(self):
+    self.x -= 9
+    self.goto(self.x, self.y)
+  def __init__(self,list = ListOfObjects):
+    super().__init__(shape="square")
+    self.penup()
+    self.color("red")
+    list.append(self)
+    screen.onkeypress(fun = self.moveright, key = "Right")
+    self.x = 0
+    self.y = 0
+    self.shapesize(stretch_wid=1.5,stretch_len=1.5)
+  def move(self):
+    pass
 bomber1= Bomber(-200,200)
 bomber2= Bomber(200,200)
 bomber3= Bomber(-200,-200)
 bomber4= Bomber(200,-200)
-while True:  
-  for  obj in ListOfObjects:
-    obj.move()
-  screen.update()
+Player = Player()
+
+
+screen.listen()
+def Game(*args):
+  screen.onkey(fun=Player.moveright, key="Right")
+  screen.onkey(fun=Player.moveleft, key="Left")
+  screen.onkey(fun=Player.moveup, key="Up")
+  screen.onkey(fun=Player.movedown, key="Down")
+  while True:
+    for obj in ListOfObjects:
+      obj.move()
+      if obj.distance(Player)<30:
+        print("GAMEOVER")
+        break
+    time.sleep(.008)
+    screen.update()
+while True:
+    screen.onclick(fun = Game)
+    screen.mainloop()
+
 screen.mainloop()
